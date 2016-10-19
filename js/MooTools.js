f7d359fbb2e878c1baa6b38952360af513df19ff
Kieran Wyse <kieranwyse@gmail.com>
2016-10-19 22:37:18 +0100

    change mootools file is blank

--- js/MooTools.js
+++ js/MooTools.js
@@ -1,26 +1,20 @@
-/* MooTools: the javascript framework. license: MIT-style license. copyright: Copyright (c) 2006-2016 [Valerio Proietti](http://mad4milk.net/).*/
-/*!
-Web Build: http://mootools.net/core/builder/e426a9ae7167c5807b173d5deff673fc
-*/
 /*
 ---
 
-name: Core
+script: NivooSlider.js
 
-description: The heart of MooTools.
+description: A nice image slider for MooTools.
 
-license: MIT-style license.
+license: MIT-style license
 
-copyright: Copyright (c) 2006-2015 [Valerio Proietti](http://mad4milk.net/).
+authors:
+- Johannes Fischer
 
-authors: The MooTools production team (http://mootools.net/developers/)
+requires:
+- core/1.4: '*'
 
-inspiration:
-  - Class implementation inspired by [Base.js](http://dean.edwards.name/weblog/2006/03/base/) Copyright (c) 2006 Dean Edwards, [GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)
-  - Some functionality inspired by [Prototype.js](http://prototypejs.org) Copyright (c) 2005-2007 Sam Stephenson, [MIT License](http://opensource.org/licenses/mit-license.php)
-
-provides: [Core, MooTools, Type, typeOf, instanceOf, Native]
+provides:
+- NivooSlider
 
 ...
-*/
-/*! MooTools: the javascript framework. license: MIT-style license. copyright: Copyright (c) 2006-2015 [Valerio Proietti](http://mad4milk.net/).*/
+ */
