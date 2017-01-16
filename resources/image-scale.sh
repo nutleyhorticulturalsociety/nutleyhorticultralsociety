2d1b3e0c76a8d407e43477e37f44bca68c1987ad
kieran <kieranwyse@gmail.com>
2017-01-16 12:25:17 +0000

    added additional gallery pictures

--- resources/image-scale.sh
+++ resources/image-scale.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-DIR="site/images/originals/*"
+#the folder that the full size pictures are stored in
+DIR="../../../Pictures/originals/*"
 
 rm image.html
 touch image.html
@@ -7,7 +8,7 @@ touch image.html
 
 for d in $DIR
 do
-	#if it is a directory	
+	#if it is a directory
 	if [ -d "$d" ]
 		then
 		echo "Processing directory... $d\n"
@@ -15,66 +16,69 @@ do
 		SUBDIR="$d/*"
 
 		#make the directory we are going to put the scaled images in
-		mkdir "site/images/${d##*/}"
-		echo "making directory site/images/${d##*/}" 
+		mkdir "../images/${d##*/}"
+		echo "making directory site/images/${d##*/}"
 		for subd in $SUBDIR
 			do
 			#if it is a directory
-			echo "Processing directory... $subd\n"	
+			echo "Processing directory... $subd\n"
 			if [ -d "$subd" ]
 				then
 
 				#make the directory we are going to put the scaled images in
-				mkdir "site/images/${d##*/}/${subd##*/}"
+				mkdir "../images/${d##*/}/${subd##*/}"
 
 				#make the directory we are going to put the icons in
-				mkdir "site/images/${d##*/}/${subd##*/}/icon"
+				mkdir "../images/${d##*/}/${subd##*/}/icon"
 
 				echo "making directory site/images/${d##*/}/${subd##*/}"
 				FILES="$subd/*"
 
-				echo "<h1>${d##*/} ${subd##*/}</h1>" >> image.html
+				section=`echo ${d##*/} | tr "[_]" " "`
+				sub_section=`echo ${subd##*/} | tr "[_]" " "`
 
-				echo "<div id="Slider" class="nivoo-slider">" >> image.html
+				echo "<h1>$section : $sub_section</h1>" >> image.html
+
+				echo "<div id="${d##*/}${subd##*/}" class="nivoo-slider">" >> image.html
 
 
-				for f in $FILES 
+				for f in $FILES
 					do
-				 	if [ -f "$f" ] 
+				 	if [ -f "$f" ]
 				 		then
 				  		echo "Processing file... $f\n"
 				  		path=${f%/*}
 				  		echo "path is:"$path"\n"
 				  		file="${f##*/}"
 				  		echo "file is:"$file"\n"
-			  			base="${f%%.*}"
+			  			base="${f%.*}"
 				  		base=`echo $base | tr "[\-| ]" "_"`
 				  		echo "base filenane is:"$base"\n"
-				  		ext="${f#*.}"
+				  		ext="${f##*.}"
 				  		echo "extension is:"$ext"\n"
-						
+						filename="${file%.*}"
+						echo "filename is:"$filename"\n"
+
+
+
 						#if we have an image
-						if [ $ext = "jpg" ]
+						if [ $ext = "jpg" ] || [ $ext = "JPG" ] ;
 							then
-				  			echo "saving:$path$base_scaled.$ext\n"
-						
-				  			convert  -resize 90x90 "$f" "site/images/${d##*/}/${subd##*/}/icon/"$file"_90x90.$ext"
-				  			convert  -resize 600 "$f" "site/images/${d##*/}/${subd##*/}/"$file"_600.$ext"
-				  			echo "<a href='#'><img src='images/${d##*/}/${subd##*/}/"$file"_600.$ext' alt='${d##*/} ${subd##*/}' height='350' width='600' /></a>" >> image.html
+				  			echo "../images/${d##*/}/${subd##*/}/"$filename"_600.$ext"
+
+				  			convert  -resize 90x90 "$f" "../images/${d##*/}/${subd##*/}/icon/"$filename"_90x90.$ext"
+				  			convert  -resize 600 "$f" "../images/${d##*/}/${subd##*/}/"$filename"_600.$ext"
+				  			echo "<a href='#'><img src='images/${d##*/}/${subd##*/}/"$filename"_600.$ext' alt='${d##*/} ${subd##*/}' height='350' width='600' /></a>" >> image.html
 						fi
-						
+
 					fi
 				done
 				echo "</div>" >> image.html
-			fi	
-			
+			fi
+
 		done
-			
+
 	fi
 
 
 done
-
-
-
-
